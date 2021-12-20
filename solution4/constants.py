1489a49179ca1b1ca10e7d9b48153a82f95894f3
Alexander Windmann <alexander.windmann@hsu-hh.de>
2021-12-20 09:47:25 +0100

    save logs in single log folder

--- solution4/constants.py
+++ solution4/constants.py
@@ -1,20 +1,21 @@
-LOGDIR = "../logs"
+LOGDIR = "../../logs"
 MODEL_NAME = "SOMVAE"
 MODEL_VERSION = 0
 BATCH_SIZE = 32
 NUM_WORKERS = 8
 USE_LOGGER = True
 
-TRAINER_CONFIG = dict(gpus=1,
-                      max_epochs=150,
-                      # ckpt_path=glob.glob("../logs/SOMVAE/version_0/checkpoints/*.ckpt")[0],
-                      )
+TRAINER_CONFIG = dict(
+    gpus=1,
+    max_epochs=150
+)
 
-HPARAMS = dict(d_som=[2, 3],
-               d_latent=64,
-               d_enc_dec=100,
-               alpha=0.75,
-               beta=2,
-               gamma=0,
-               tau=0
-               )
+HPARAMS = dict(
+    d_som=[2, 3],
+    d_latent=64,
+    d_enc_dec=100,
+    alpha=0.75,
+    beta=2,
+    # gamma=0,
+    # tau=0
+)
